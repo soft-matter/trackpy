9facd35883e8c0551435fb46e9eefcfe72d04123
Thomas A Caswell <tcaswell@gmail.com>
2012-07-24 10:24:17 -0500

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+print "Installing trackpy"
+
+setup(
+    name='trackpy',
+    version='0.1',
+    author='Thomas A Caswell',
+    author_email='tcaswell@uchicago.edu',
+    url='https://github.com/tacaswell/trackpy',
+    packages=['trackpy'],
+    )
