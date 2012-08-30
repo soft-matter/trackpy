5a03112a8d368fdd4f6137b66efd32308c3a7c5d
Dan Allan <dallan@pha.jhu.edu>
2012-08-30 01:01:08 -0400

    minor logging change. found an unsolved bug with mux age -e

--- mux.py
+++ mux.py
@@ -263,7 +263,7 @@ class ParseTime(argparse.Action):
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
                     datefmt='%m-%d %H:%M',
-                    filename='example.log')
+                    filename='mux.log')
 # define a Handler which writes INFO messages or higher to the sys.stderr
 console = logging.StreamHandler()
 console.setLevel(logging.INFO)
