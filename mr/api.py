a729324051d5e15bbd4549caf19b01f6ff50ee85
Dan Allan <daniel.b.allan@gmail.com>
2012-09-19 15:40:16 -0400

    api typo

--- mr/api.py
+++ mr/api.py
@@ -1,7 +1,7 @@
 from feature import locate, sample, batch, bandpass
 from diagnostics import annotate, subpx_hist
 from motion import (drift, subtract_drift, msd, ensemble_msd, split_by_probe,
-                    is_unphysical, is_localized, is_diffusive, idl_track
+                    is_unphysical, is_localized, is_diffusive, idl_track,
                     cast_probes)
 from plots import (plot_msd, plot_emsd, plot_bimodal_msd, plot_drift, plot_traj)
 from viscosity import fischer
