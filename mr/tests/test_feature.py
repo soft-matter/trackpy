f2dcf92e23f0d7cc96e58caedac54a3fc83cc1aa
Dan Allan <daniel.b.allan@gmail.com>
2012-09-19 18:38:47 -0400

    bandpass testing and README

--- mr/tests/test_feature.py
+++ mr/tests/test_feature.py
@@ -1,11 +1,20 @@
 import nose
 import unittest
 
+import numpy as np
+
 from mr import feature
+
+SAMPLE_FRAME = feature.plt.imread('data/sample_frame.png')
 
 class TestFeature(unittest.TestCase):
 
-    SAMPLE_IMAGE = 'data/sample_frame.png'
-
     def test_bandpass(self):
-        
+        result = feature.bandpass(SAMPLE_FRAME, 1, 12)
+        expect = np.loadtxt('data/bandpass_result.txt')
+        self.assert_(np.allclose(result, expect))
+
+if __name__ == '__main__':
+    import nose
+    nose.runmodule(argv=[__file__, '-vv', '-s', '-x'], exit=False)
+
