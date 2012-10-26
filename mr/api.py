1c9737b7edf01445fd3e06012cfd486705d6ee75
Dan Allan <daniel.b.allan@gmail.com>
2012-10-26 17:40:10 -0400

    updated split, stack in API

--- mr/api.py
+++ mr/api.py
@@ -23,7 +23,7 @@ from datetime import datetime, timedelta
 
 from feature import locate, sample, batch, bandpass
 from diagnostics import annotate, subpx_hist
-from motion import (drift, subtract_drift, msd, ensemble_msd, split_by_probe,
+from motion import (drift, subtract_drift, msd, ensemble_msd, split, stack,
                     is_unphysical, is_localized, is_diffusive, idl_track,
                     cast_probes)
 from plots import (plot_msd, plot_emsd, plot_bimodal_msd, plot_drift, plot_traj)
