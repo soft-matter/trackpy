3a6c9fb2238c74e2ab810495fda6cdf81ce7bd29
Thomas A Caswell <tcaswell@uchicago.edu>
2013-01-28 12:39:57 -0600

    added intersphinx map for numpy

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -246,4 +246,7 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'http://docs.python.org/': None}
+
+intersphinx_mapping = {'python': ('http://docs.python.org/2.7', None),
+                       'numpy': ('http://docs.scipy.org/doc/numpy',None),
+                       }
