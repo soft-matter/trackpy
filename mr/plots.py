38bdec00aaf8cf937cbea49b56850d583107c356
Dan Allan <daniel.b.allan@gmail.com>
2013-03-25 20:40:43 -0400

    wire analysis works

--- mr/plots.py
+++ mr/plots.py
@@ -204,3 +204,25 @@ def fit(data, fits, inverted_model=False, logx=False, logy=False, ax=None):
     if logx:
         ax.set_xscale('log') # logx kwarg does not always take. Bug?
     return ax
+
+@make_axes
+def plot_princple_axes(x_bar, y_bar, cov, ax=None):
+    """Plot bars with a length of 2 stddev along the principal axes.
+
+    Attribution
+    -----------
+    This function is based on a solution by Joe Kington, posted on Stack
+    Overflow at http://stackoverflow.com/questions/5869891/
+    how-to-calculate-the-axis-of-orientation/5873296#5873296
+    """
+
+    def make_lines(eigvals, eigvecs, mean, i):
+        """Make lines a length of 2 stddev."""
+        std = np.sqrt(eigvals[i])
+        vec = 2 * std * eigvecs[:,i] / np.hypot(*eigvecs[:,i])
+        x, y = np.vstack((mean-vec, mean, mean+vec)).T
+        return x, y
+    mean = np.array([x_bar, y_bar])
+    eigvals, eigvecs = np.linalg.eigh(cov)
+    ax.plot(*make_lines(eigvals, eigvecs, mean, 0), marker='o', color='white')
+    ax.plot(*make_lines(eigvals, eigvecs, mean, -1), marker='o', color='green')
--- mr/wire.py
+++ mr/wire.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy import  ndimage
+from mr.kington import *
 
 def threshold(im, sigma=3):
     """Threshold a grayscale image based on the mean and std brightness.
@@ -10,25 +11,89 @@ def threshold(im, sigma=3):
     sigma: float, default 3.0
         minimum brightness in terms of standard deviations above the mean
     """
-    mask = im > (im.mean() + 3*im.std())
+    mask = im > (im.mean() + sigma*im.std())
     return mask
 
-def bigfish(mask):
+def bigfish(mask, padding=0.02):
     """Identify the largest connected region and return the roi. 
 
     Parameters
     ----------
     mask: binary (thresholded) image
+    padding: fractional padding of ROI (default 0.02)
+
+    Returns
+    -------
+    padded_roi: a tuple of slice objects, for indexing the image
     """
     label_im, nb_labels = ndimage.label(mask)
     sizes = ndimage.sum(mask, label_im, range(nb_labels + 1))
     big_label = sizes.argmax() # the label of the largest connection region
     roi = ndimage.find_objects(label_im==big_label)[0]
-    return roi
+    padded_roi = pad_roi(roi, padding, mask.shape)
+    return padded_roi
 
-def orientation(img):
-    i, j = np.mgrid[:img.shape[0], :img.shape[1]]
-    coords = np.vstack((i.reshape(-1), j.reshape(-1), img.reshape(-1))).T
-    covariance = np.cov(coords)
-    eigvals, eigvecs = np.linalg.eigh(covariance)
-    return eigvecs
+def pad_roi(roi, padding, img_shape):
+    s0, s1 = roi # slices in x and y
+    p = int(np.max(img_shape)*padding)
+    new_s0 = slice(np.clip(s0.start - p, 0, img_shape[0] - 1),
+                   np.clip(s0.stop + p, 0, img_shape[0] - 1))
+    new_s1 = slice(np.clip(s1.start - p, 0, img_shape[1] - 1),
+                   np.clip(s1.stop + p, 0, img_shape[1] - 1))
+    return new_s0, new_s1
+
+def filter(img, sigma=1):
+    """Mask the original image using a gently thresholded image, in
+    preparation for a fit."""
+    return np.where(threshold(img, sigma), img, np.zeros_like(img))
+
+def moment(data, i, j):
+    """Utility function called by inertial_axes. See that function, below,
+    for attribution and usage."""
+    nrows, ncols = data.shape
+    y, x = np.mgrid[:nrows, :ncols]
+    return (data * x**i * y**j).sum()
+
+def inertial_axes(data):
+    """Calculate the x-mean, y-mean, and cov matrix of an image.
+    Parameters
+    ----------
+    img: ndarray
+    
+    Returns
+    -------
+    xbar, ybar, cov (the covariance matrix)
+
+    Attribution
+    -----------
+    This function is based on a solution by Joe Kington, posted on Stack
+    Overflow at http://stackoverflow.com/questions/5869891/
+    how-to-calculate-the-axis-of-orientation/5873296#5873296"""
+
+    normalization = data.sum()
+    x_bar = moment(data, 1, 0) / normalization
+    y_bar = moment(data, 0, 1) / normalization
+    u11 = moment(data, 1, 1) / normalization - x_bar*y_bar
+    u20 = moment(data, 2, 0) / normalization - x_bar**2
+    u02 = moment(data, 0, 2) / normalization - y_bar**2
+    cov = np.array([[u20, u11], [u11, u02]])
+    return x_bar, y_bar, cov
+
+def angle(cov):
+    """Compute the orientation angle of the largest principal axis of
+    a covariance matrix.
+
+    Parameters
+    ----------
+    cov: 2x2 ndarray
+
+    Returns
+    -------
+    angle in radians
+    """
+
+    eigvals, eigvecs = np.linalg.eigh(cov)
+    eigvec = eigvecs[eigvals.argmax()] # dominant eigenvector
+    angle = np.arctan2(eigvec[1], eigvec[0])
+    return angle
+
