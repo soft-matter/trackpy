19d9860db2c79314554dcffe9dd364a79ae0c914
Dan Allan <daniel.b.allan@gmail.com>
2013-05-24 16:05:47 -0400

    CLN: Remove code that was in the wrong file.

--- mr/models/viscous.py
+++ mr/models/viscous.py
@@ -19,31 +19,6 @@ def params_as_dict(func):
     return wrapper
 
 
-def setup_params(angle):
-    """The fit parameters' bounds depend on the range of the angle data.
-    
-    Parameters
-    ----------
-    angle : an array-like sequence of angle values
-
-    Returns
-    -------
-    params : Parameters object with correct bounds
-    """
-    pi = np.pi
-    params = lmfit.Parameters()
-    params.add('m', value=1.7, min=0)
-    params.add('C', value=1.0, min=0)
-    params.add('offset', pi/100) # specify bounds below 
-    params.add('theta0_plus_offset', pi/50, min=0, max=0.2) # bounding utility 
-    params.add('theta0', expr='theta0_plus_offset - offset')
-
-    offset = params['offset']
-    angle_min, angle_max = angle.min(), angle.max()
-    assert angle_min.size == 1, "argument must be a 1D sequence"
-    offset.min, offset.max = -pi/2 - angle_min, pi/2 - angle_max
-    return params
-
 @params_as_dict
 def model(t, params):
     """Model a wire rotated in a power fluid.
