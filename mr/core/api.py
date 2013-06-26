d295cf6ca478431e7dc0511e05053546205ec7b8
Dan Allan <daniel.b.allan@gmail.com>
2013-06-26 16:15:38 -0400

    Import sql module if MySQL is available. Old overly specific SQL function have been removed.

--- mr/core/api.py
+++ mr/core/api.py
@@ -1,6 +1,14 @@
-from mr.core.feature import locate, sample, batch
+from mr.core.feature import locate, batch
+from mr.core.preprocessing import bandpass
 from mr.core.motion import (compute_drift, subtract_drift, imsd, emsd, vanhove,
                     is_typical, is_not_dirt, direction_corr)
 from mr.core.tracking import track, bust_ghosts, bust_clusters
 from mr.core.plots import *
 from mr.core.utils import fit_powerlaw
+
+try:
+    import MySQLdb
+except ImportError:
+    pass # silently, in this case
+else:
+    from mr.core import sql
