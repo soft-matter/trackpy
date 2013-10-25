694afabeffdd8a23bf299ce00947dd96cd9c5807
danielballan <daniel.b.allan@gmail.com>
2013-10-25 00:17:49 -0400

    ENH: Link features in an HDFStore on unlimited size using LinkOnDis.

--- mr/core/linking.py
+++ mr/core/linking.py
@@ -81,7 +81,7 @@ def link_iterator(numbered_frames, search_range, hash_size, memory=0,
     hash_size : Dimensions of the search space, inferred from data by default.
     box_size : A parameter of the underlying algorith, defaults to same
         as search_range, which gives reasonably optimal performance.
-    position_cols : DataFrame column names (unlimited dimensions)
+    pos_columns: DataFrame column names (unlimited dimensions)
 
     Returns
     -------
@@ -122,4 +122,95 @@ Proceed with caution.""" % frame_no,
             """Some probes were not labeled in Frame %d. 
 Missed probes are labeled -1. Proceed with caution.""" % frame_no,
             UserWarning)
+
+CHUNK_SIZE=1000
+
+class LinkOnDisk(object):
+    """This helper class manages the process of linking trajectories
+    by loading frames one at a time and saving the results one frame
+    at a time. This allows infinitely long videos to be analyzed
+    using only a modest amount of memory.
+    """
+
+    def __init__(self, filename, key, pos_columns=['x', 'y'], max_frame=None):
+        self.filename = filename
+        self.key = key
+        self.pos_columns = pos_columns
+        if max_frame is None:
+            self.last_frame = self.detect_last_frame()
+            print "Detected last frame is Frame %d" % self.last_frame 
+        else:
+            self.last_frame = max_frame
+        # We are assuming that the first frame is Frame 0. If we're wrong,
+        # we only waste a little time in the loop.
+        self.first_frame = 0
+        self.size = self.detect_size() # for hash
+
+
+    def link(self, search_range, memory=0, hash_size=None, box_size=None):
+        if hash_size is None:
+            MARGIN = 1  # to avoid OutOfHashException
+            hash_size = self.size + 1
+        numbered_frames = self.numbered_frames()
+        label_generator = \
+            link_iterator(numbered_frames, search_range, hash_size,
+                          memory, box_size, self.pos_columns)
+        self.label_generator = label_generator
+
+    def save(self, out_filename, out_key):
+        with pd.get_store(out_filename) as out_store:
+            with pd.get_store(self.filename) as store:
+                while True:
+                    try:
+                        frame_no, labels = next(self.label_generator)
+                    except StopIteration:
+                        break
+                    # Fetch data (redundantly) this time taking all columns.
+                    frame = store.select(self.key, 'frame == %d' % frame_no)
+                    frame['probe'] = -1  # an integer placeholder
+                    frame['probe'].update(labels)
+                    out_store.append(out_key, frame)
+                    print "Frame %d written." % frame_no
+
+    def numbered_frames(self):
+        with pd.get_store(self.filename) as store:
+            for frame_no in xrange(self.first_frame, 1 + self.last_frame):
+                frame = store.select(self.key, 'frame == %d' % frame_no, 
+                                     columns=self.pos_columns)
+                yield frame_no, frame
+
+    def detect_last_frame(self):
+        with pd.get_store(self.filename) as store:
+            last_frame = 0
+            for chunk in store.select_column(self.key, 'frame', iterator=True):
+                chunk_max = chunk.max()
+                if chunk_max > last_frame:
+                    last_frame = chunk_max
+        return last_frame
+
+    def detect_size(self):
+        with pd.get_store(self.filename) as store:
+            size = np.zeros(len(self.pos_columns))
+            for chunk in store.select(self.key, iterator=True,
+                                      columns=self.pos_columns):
+                size = np.maximum(size, chunk.max())
+        return size.values 
+            
+
+def numbered_frames_from_sql(table, conn, sql_flavor,
+                             pos_columns=['x', 'y']):
+    pos_columns = ', '.join(pos_columns)
+    frame_nos = pd.read_sql("SELECT DISTINCT FRAME FROM %s" % table, conn) 
+    frame_nos = frame_nos.values.astype('int')
+    for frame_no in frame_nos:
+        frame = pd.read_sql("SELECT %s FROM %s WHERE FRAME = %d" %
+                              (pos_columns, table, frame_no), conn) 
+        yield frame_no, frame
+
+
+def labeled_frame_to_sql(frame_no_and_labels, table, conn, sql_flavor):
+    frame_no, labels = frame_no_and_labels
+    frame['frame'] = frame_no
+    frame.to_sql(table, conn, sql_flavor, if_exists='append')
+
 track = link # legacy
