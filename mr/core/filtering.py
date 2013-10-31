04413b88aeb25fbaed6ef1fb9cc15c1a5519864e
danielballan <daniel.b.allan@gmail.com>
2013-10-31 14:32:00 -0400

    Trajectories class to manage chunking of trajectory DataFrames.

--- mr/core/filtering.py
+++ mr/core/filtering.py
@@ -2,7 +2,7 @@
 by wrapping pandas group-by and filter capabilities."""
 
 
-def bust_ghosts(tracks, threshold=100):
+def filter_stubs(tracks, threshold=100):
     """Filter out trajectories with few points. They are often specious.
 
     Parameters
@@ -23,7 +23,7 @@ def bust_ghosts(tracks, threshold=100):
     filtered = grouped.filter(lambda x: x.frame.count() >= threshold)
     return filtered.set_index('frame', drop=False)
 
-def bust_clusters(tracks, quantile=0.8, threshold=None):
+def filter_clusters(tracks, quantile=0.8, threshold=None):
     """Filter out trajectories with a mean probe size above a given quantile.
 
     Parameters
@@ -40,3 +40,6 @@ def bust_clusters(tracks, quantile=0.8, threshold=None):
         threshold = tracks['size'].quantile(quantile)
     f = lambda x: x['size'].mean() < threshold # filtering function
     return tracks.groupby('probe').filter(f)
+
+bust_ghosts = filter_stubs
+bust_clusters = filter_clusters
