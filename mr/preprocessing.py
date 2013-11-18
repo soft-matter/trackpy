ee56334f67b884c7588511af8569bc46f4e026a0
danielballan <daniel.b.allan@gmail.com>
2013-11-18 17:04:50 -0500

    ENH: Enable FFTW cache.

--- mr/preprocessing.py
+++ mr/preprocessing.py
@@ -4,20 +4,24 @@ from scipy.ndimage.fourier import fourier_gaussian
 import warnings
 
 
-first_run = True
+# When loading module, try to use pyFFTW ("Fastest Fourier Transform in the
+# West") if it is available.
 try:
     import pyfftw
 except ImportError:
+    # Use numpy.
     fftn = np.fft.fftn
     ifftn = np.fft.ifftn
 else:
+    pyfftw.interfaces.cache.enable()
+    planned = False
     def _maybe_align(a):
         global planned
-        if first_run:
+        if not planned:
             warnings.warn("FFTW is configuring itself. This will take " +
                           "several sections, but subsequent calls will run " +
                           "*much* faster.", UserWarning)
-        planned = False
+            planned = True 
         return pyfftw.n_byte_align(a, a.dtype.alignment)
     fftn = lambda a: pyfftw.interfaces.numpy_fft.fftn(_maybe_align(a))
     ifftn = lambda a: pyfftw.interfaces.numpy_fft.ifftn(_maybe_align(a))
