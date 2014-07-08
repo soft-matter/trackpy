FIX/ENH: Allow locate to invert float images by assuming normalization=1.

trackpy/feature.py
@@ -397,7 +397,7 @@ def _numba_refine(raw_image, image, radius, coords, max_iterations,
 
 
 def locate(raw_image, diameter, minmass=100., maxsize=None, separation=None,
-           noise_size=1, smoothing_size=None, threshold=1, invert=False,
+           noise_size=1, smoothing_size=None, threshold=None, invert=False,
            percentile=64, topn=None, preprocess=True, max_iterations=10,
            filter_before=True, filter_after=True,
            characterize=True, engine='auto'):
@@ -423,7 +423,7 @@ def locate(raw_image, diameter, minmass=100., maxsize=None, separation=None,
     smoothing_size : size of boxcar smoothing, in pixels
         Default is the same is feature separation.
     threshold : Clip bandpass result below this value.
-        Default 1; use 8 for 16-bit images.
+        Default None, passed through to bandpass.
     invert : Set to True if features are darker than background. False by
         default.
     percentile : Features must have a peak brighter than pixels in this
@@ -489,8 +489,13 @@ def locate(raw_image, diameter, minmass=100., maxsize=None, separation=None,
         if invert:
             # It is tempting to do this in place, but if it is called multiple
             # times on the same image, chaos reigns.
-            max_value = np.iinfo(raw_image.dtype).max
-            raw_image = raw_image ^ max_value
+            if np.issubdtype(raw_image.dtype, np.integer):
+                max_value = np.iinfo(raw_image.dtype).max
+                raw_image = raw_image ^ max_value
+            else:
+                # To avoid degrading performance, assume gamut is zero to one.
+                # Have you ever encountered an image of unnormalized floats?
+                raw_image = 1 - raw_image
         image = bandpass(raw_image, noise_size, smoothing_size, threshold)
     else:
         image = raw_image.copy()
@@ -591,7 +596,7 @@ def locate(raw_image, diameter, minmass=100., maxsize=None, separation=None,
 
 
 def batch(frames, diameter, minmass=100, maxsize=None, separation=None,
-          noise_size=1, smoothing_size=None, threshold=1, invert=False,
+          noise_size=1, smoothing_size=None, threshold=None, invert=False,
           percentile=64, topn=None, preprocess=True, max_iterations=10,
           filter_before=True, filter_after=True,
           characterize=True, engine='auto',
@@ -618,7 +623,7 @@ def batch(frames, diameter, minmass=100, maxsize=None, separation=None,
     smoothing_size : size of boxcar smoothing, in pixels
         Default is the same is feature separation.
     threshold : Clip bandpass result below this value.
-        Default 1; use 8 for 16-bit images.
+        Default None, passed through to bandpass.
     invert : Set to True if features are darker than background. False by
         default.
     percentile : Features must have a peak brighter than pixels in this

trackpy/tests/test_feature.py
@@ -6,6 +6,7 @@ from pandas import DataFrame, Series
 import trackpy as tp
 from trackpy.try_numba import NUMBA_AVAILABLE
 
+import warnings
 import unittest
 import nose
 from numpy.testing import assert_almost_equal, assert_allclose
@@ -25,7 +26,7 @@ def draw_gaussian_spot(image, pos, r, max_value=None, ecc=0):
     if max_value is None:
         max_value = np.iinfo(image.dtype).max - 3
     spot = max_value*np.exp(-((x/(1 - ecc))**2 + (y*(1 - ecc))**2)/(2*r**2)).T
-    image += spot
+    image += spot.astype(image.dtype)
 
 
 def gen_random_locations(shape, count):
@@ -55,16 +56,23 @@ class CommonFeatureIdentificationTests(object):
     def check_skip(self):
         pass
 
-    def test_smoke_test(self):
+    def test_smoke_datatypes(self):
         self.check_skip()
+        SHAPE = (300, 300)
         # simple "smoke" test to see if numba explodes
-        dummy_image = np.random.randint(0, 100, (300, 300)).astype(np.uint8)
+        dummy_image = np.random.randint(0, 100, SHAPE).astype(np.uint8)
+        tp.locate(dummy_image, 5, engine=self.engine)
+        tp.locate(dummy_image, 5, invert=True, engine=self.engine)
+
+        # Check float types
+        dummy_image = np.random.rand(*SHAPE)
         tp.locate(dummy_image, 5, engine=self.engine)
         tp.locate(dummy_image, 5, invert=True, engine=self.engine)
 
     def test_black_image(self):
         self.check_skip()
         black_image = np.zeros((21, 23)).astype(np.uint8)
+        warnings.simplefilter('always')
         with assert_produces_warning(UserWarning):
             f = tp.locate(black_image, 5, engine=self.engine, preprocess=False)
 