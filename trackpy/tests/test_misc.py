54dd75d1056623314818609c72e5e095fbfce31c
danielballan <daniel.b.allan@gmail.com>
2014-12-12 13:34:45 -0500

    TST: Test logging convenience functions.

--- trackpy/tests/test_misc.py
+++ trackpy/tests/test_misc.py
@@ -2,6 +2,7 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 import six
 import os
+import logging
 import unittest
 import warnings
 
@@ -36,3 +37,26 @@ class APITests(unittest.TestCase):
                 for wrn in w:
                     print(wrn, wrn.message)
             assert len(w) == 1
+
+
+class LoggerTests(unittest.TestCase):
+    def test_heirarchy(self):
+        self.assertTrue(trackpy.linking.logger.parent is trackpy.logger)
+        self.assertTrue(trackpy.feature.logger.parent is trackpy.logger)
+        self.assertTrue(trackpy.preprocessing.logger.parent is trackpy.logger)
+
+    def test_convenience_funcs(self):
+        trackpy.quiet(True)
+        self.assertEqual(trackpy.logger.level, logging.WARN)
+        trackpy.quiet(False)
+        self.assertEqual(trackpy.logger.level, logging.INFO)
+
+        trackpy.ignore_logging()
+        self.assertEqual(len(trackpy.logger.handlers), 0)
+        self.assertEqual(trackpy.logger.level, logging.NOTSET)
+        self.assertTrue(trackpy.logger.propagate)
+
+        trackpy.handle_logging()
+        self.assertEqual(len(trackpy.logger.handlers), 1)
+        self.assertEqual(trackpy.logger.level, logging.INFO)
+        self.assertEqual(trackpy.logger.propagate, 1)
