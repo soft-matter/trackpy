dcad1c1ac976beaa694c768c50a6dc514f714ffe
Nathan Keim <nkeim@calpoly.edu>
2015-03-01 18:09:17 -0800

    BUG: Prevent pims namespace conflicts.

--- trackpy/api.py
+++ trackpy/api.py
@@ -1,6 +1,11 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 import six
+
+# Import all of pims top-level for convenience. But do it first so
+# that trackpy names take precedence.
+from pims import *
+
 from .motion import *
 from .plots import *
 from .linking import *
@@ -10,6 +15,3 @@ from .preprocessing import bandpass
 from .framewise_data import *
 from . import utils
 from .try_numba import try_numba_autojit, enable_numba, disable_numba
-
-# Import all of pims top-level for convenience.
-from pims import *
