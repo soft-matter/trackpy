50696a8e1f45d1430c2a6ed540bd7f19f681346e
Nathan Keim <nkeim@calpoly.edu>
2015-03-02 14:32:39 -0800

    API: Added deprecation warning wrapper to pims imports

--- trackpy/api.py
+++ trackpy/api.py
@@ -1,10 +1,29 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 import six
+import warnings
 
-# Import all of pims top-level for convenience. But do it first so
-# that trackpy names take precedence.
-from pims import *
+# pims import is deprecated. We include it here for backwards
+# compatibility, but there will be warnings.
+import pims as _pims
+
+
+def _deprecate_pims(call):
+    """Wrap a pims callable with a warning that it is deprecated."""
+    def deprecated_pims_import(*args, **kw):
+        """Class imported from pims package. Its presence in trackpy is deprecated."""
+        warnings.warn(('trackpy.{0} is being called, but "{0}" is really part of the '
+                      'pims package. It will not be in future versions of trackpy. '
+                      'Consider importing pims and calling pims.{0} instead.'
+                      ).format(call.__name__), UserWarning)
+        return call(*args, **kw)
+    return deprecated_pims_import
+
+
+ImageSequence = _deprecate_pims(_pims.ImageSequence)
+Video = _deprecate_pims(_pims.Video)
+TiffStack = _deprecate_pims(_pims.TiffStack)
+
 
 from .motion import *
 from .plots import *
--- trackpy/tests/test_misc.py
+++ trackpy/tests/test_misc.py
@@ -1,10 +1,14 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 import six
+import os
 import unittest
+import warnings
 
+import trackpy
 import trackpy.diag
 
+path, _ = os.path.split(os.path.abspath(__file__))
 
 class DiagTests(unittest.TestCase):
     def test_performance_report(self):
@@ -12,3 +16,11 @@ class DiagTests(unittest.TestCase):
 
     def test_dependencies(self):
         trackpy.diag.dependencies()
+
+
+class APITests(unittest.TestCase):
+    def test_pims_deprecation(self):
+        with warnings.catch_warnings(True) as w:
+            warnings.simplefilter('always')
+            _ = trackpy.ImageSequence(os.path.join(path, 'video/image_sequence/*.png'))
+            assert len(w) == 1
