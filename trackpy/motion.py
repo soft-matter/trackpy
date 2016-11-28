bd655ed6a1e93d6356fb5d03a6c25be3b4550fd9
Thierry <thierry.bottaro@gmail.com>
2016-11-28 21:21:18 +0100

    Update motion.py

--- trackpy/motion.py
+++ trackpy/motion.py
@@ -260,7 +260,7 @@ def compute_drift(traj, smoothing=0, pos_columns=None):
     # I do here a copy because a "inplace=True" would sort the original "traj" which is perhaps unwanted/unexpected
     traj = pandas_sort(traj,'frame')
     # Probe by particle, take the difference between frames.
-    delta = traj.groupby('particle').apply(lambda x :
+    delta = traj.groupby('particle',sort=False).apply(lambda x :
                                     x.set_index('frame', drop=False).diff())
     # Keep only deltas between frames that are consecutive.
     delta = delta[delta['frame'] == 1]
