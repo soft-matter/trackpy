65ddf2b824898caf1adb754394e0b4c6026b8f93
Nathan Keim <nkeim@calpoly.edu>
2017-04-02 17:59:56 -0700

    TST: Fix prediction tests with "New" linker by using int frame nos.

--- trackpy/tests/test_predict.py
+++ trackpy/tests/test_predict.py
@@ -16,8 +16,8 @@ from .test_find_link import CoordinateReader
 
 def mkframe(n=1, Nside=3):
     xg, yg = np.mgrid[:Nside,:Nside]
-    dx = (n - 1)
-    dy = -(n - 1)
+    dx = (n/100. - 1)
+    dy = -(n/100. - 1)
     return pandas.DataFrame(
             dict(x=xg.flatten() + dx, y=yg.flatten() + dy, frame=n))
 
@@ -29,7 +29,10 @@ def link(frames, linker, *args, **kw):
 
 def get_linked_lengths(frames, linker, *args, **kw):
     """Track particles and return the length of each trajectory."""
-    return link(frames, linker, *args, **kw).groupby('particle').x.count()
+    print(frames)
+    linked = link(frames, linker, *args, **kw)
+    print(linked)
+    return linked.groupby('particle').x.count()
 
 Nside_oversize = int(np.sqrt(100)) # Make subnet linker fail
 
@@ -57,19 +60,19 @@ class BaselinePredictTests(LinkWithPrediction, StrictTestCase):
         """
         # link_df_iter
         pred = predict.NullPredict()
-        ll = get_linked_lengths((mkframe(0), mkframe(0.25)),
+        ll = get_linked_lengths((mkframe(0), mkframe(25)),
                                 self.get_linker_iter(pred), 0.45)
         assert all(ll.values == 2)
 
         # link_df
         pred = predict.NullPredict()
         ll_df = self.get_linker(pred)(pandas.concat((mkframe(0),
-                                                     mkframe(0.25))), 0.45)
+                                                     mkframe(25))), 0.45)
         assert all(ll_df.groupby('particle').x.count().values == 2)
 
         # Make sure that keyword options are handled correctly
         # (This checks both link_df and link_df_iter)
-        features = pandas.concat((mkframe(0), mkframe(0.25)))
+        features = pandas.concat((mkframe(0), mkframe(25)))
         features.rename(columns=lambda n: n + '_', inplace=True)
         pred = predict.NullPredict()
         ll_df = self.get_linker(pred)(features, 0.45, t_column='frame_',
@@ -82,12 +85,12 @@ class BaselinePredictTests(LinkWithPrediction, StrictTestCase):
         """
         pred = predict.null_predict
         pred_link = functools.partial(self.get_unwrapped_linker(), predictor=pred)
-        ll = get_linked_lengths((mkframe(0), mkframe(0.25)),
+        ll = get_linked_lengths((mkframe(0), mkframe(25)),
                                 pred_link, 0.45)
         assert all(ll.values == 2)
 
     def test_fail_predict(self):
-        ll = get_linked_lengths((mkframe(0), mkframe(0.25), mkframe(0.65)),
+        ll = get_linked_lengths((mkframe(0), mkframe(25), mkframe(65)),
                                 self.get_unwrapped_linker(), 0.45)
         assert not all(ll.values == 2)
 
@@ -95,60 +98,60 @@ class BaselinePredictTests(LinkWithPrediction, StrictTestCase):
     def test_subnet_fail(self):
         Nside = Nside_oversize
         ll = get_linked_lengths((mkframe(0, Nside),
-                                 mkframe(0.25, Nside),
-                                 mkframe(0.75, Nside)), self.get_unwrapped_linker(), 1)
+                                 mkframe(25, Nside),
+                                 mkframe(75, Nside)), self.get_unwrapped_linker(), 1)
 
 class VelocityPredictTests(LinkWithPrediction):
     def test_simple_predict(self):
         pred = self.predict_class()
-        ll = get_linked_lengths((self.mkframe(0), self.mkframe(0.25), self.mkframe(0.65)),
+        ll = get_linked_lengths((self.mkframe(0), self.mkframe(25), self.mkframe(65)),
                                 self.get_linker_iter(pred), 0.45)
         assert all(ll.values == 3)
 
     def test_big_predict(self):
         Nside = Nside_oversize
         pred = self.predict_class()
-        ll = get_linked_lengths((self.mkframe(0, Nside), self.mkframe(0.25, Nside),
-                                 self.mkframe(0.75, Nside)),
+        ll = get_linked_lengths((self.mkframe(0, Nside), self.mkframe(25, Nside),
+                                 self.mkframe(75, Nside)),
                                 self.get_linker_iter(pred), 0.45)
         assert all(ll.values == 3)
 
     def test_predict_newparticles(self):
         """New particles should get the velocities of nearby ones."""
         pred = self.predict_class()
-        ll = get_linked_lengths((self.mkframe(0), self.mkframe(0.25), self.mkframe(0.65, 4),
-                                self.mkframe(1.05, 4)),
+        ll = get_linked_lengths((self.mkframe(0), self.mkframe(25), self.mkframe(65, 4),
+                                self.mkframe(105, 4)),
                                 self.get_linker_iter(pred), 0.45)
         assert not any(ll.values == 1)
 
     def test_predict_memory(self):
         pred = self.predict_class()
-        frames = [self.mkframe(0), self.mkframe(0.25), self.mkframe(0.65),
-                  self.mkframe(1.05), self.mkframe(1.45)]
+        frames = [self.mkframe(0), self.mkframe(25), self.mkframe(65),
+                  self.mkframe(105), self.mkframe(145)]
         ll = get_linked_lengths(frames, self.get_linker_iter(pred), 0.45)
         assert all(ll.values == len(frames))
 
         # Knock out a particle. Make sure tracking fails.
-        frames[3].x[5] = np.nan
+        frames[3].loc[5, 'x'] = np.nan
         frames[3] = frames[3].dropna()
         pred = self.predict_class()
         tr = link(frames, self.get_linker_iter(pred), 0.45)
         starts = tr.groupby('particle').frame.min()
         ends = tr.groupby('particle').frame.max()
-        assert not all(ends - starts == 1.45)
+        assert not all(ends - starts == 145)
 
         pred = self.predict_class()
         tr = link(frames, self.get_linker_iter(pred), 0.45, memory=1)
         starts = tr.groupby('particle').frame.min()
         ends = tr.groupby('particle').frame.max()
-        assert all(ends - starts == 1.45), 'Prediction with memory fails.'
+        assert all(ends - starts == 145), 'Prediction with memory fails.'
 
     def test_predict_diagnostics(self):
         """Minimally test predictor instrumentation."""
         pred = self.instrumented_predict_class()
         Nside = Nside_oversize
-        frames = (self.mkframe(0, Nside), self.mkframe(0.25, Nside),
-                  self.mkframe(0.75, Nside))
+        frames = (self.mkframe(0, Nside), self.mkframe(25, Nside),
+                  self.mkframe(75, Nside))
         ll = get_linked_lengths(frames, self.get_linker_iter(pred), 0.45)
         assert all(ll.values == 3)
         diags = pred.dump()
@@ -170,9 +173,9 @@ class NearestVelocityPredictTests(VelocityPredictTests, StrictTestCase):
         in the first pair of frames may be large."""
         pred = self.predict_class(
             initial_guess_positions=[(0., 0.)],
-            initial_guess_vels=[(1., -1.)])
-        ll = get_linked_lengths((self.mkframe(0), self.mkframe(1.),
-                                 self.mkframe(2.)),
+            initial_guess_vels=[(0.01, -0.01)])
+        ll = get_linked_lengths((self.mkframe(0), self.mkframe(100),
+                                 self.mkframe(200)),
                                 self.get_linker_iter(pred), 0.45)
         assert all(ll.values == 3)
 
@@ -185,9 +188,9 @@ class DriftPredictTests(VelocityPredictTests, StrictTestCase):
     def test_initial_guess(self):
         """When an accurate initial velocity is given, velocities
         in the first pair of frames may be large."""
-        pred = self.predict_class(initial_guess=(1., -1.))
-        ll = get_linked_lengths((self.mkframe(0), self.mkframe(1.),
-                                 self.mkframe(2.)),
+        pred = self.predict_class(initial_guess=(0.01, -0.01))
+        ll = get_linked_lengths((self.mkframe(0), self.mkframe(100),
+                                 self.mkframe(200)),
                                 self.get_linker_iter(pred), 0.45)
         assert all(ll.values == 3)
 
@@ -201,29 +204,32 @@ class ChannelPredictXTests(VelocityPredictTests, StrictTestCase):
         self.mkframe = self._channel_frame
     def _channel_frame(self, n=1, Nside=3):
         xg, yg = np.mgrid[:Nside,:Nside]
-        dx = (n - 1) * np.sqrt(2)
+        dx = (n / 100. - 1) * np.sqrt(2)
         dy = 0.
         return pandas.DataFrame(
                 dict(x=xg.flatten() + dx, y=yg.flatten() + dy, frame=n))
+
     def test_initial_guess(self):
         """When an accurate initial velocity profile is given, velocities
         in the first pair of frames may be large."""
         def _shear_frame(t=1., Nside=4):
             xg, yg = np.mgrid[:Nside,:Nside]
-            dx = 0.45 * t * yg
+            dx = 0.0045 * t * yg
             return pandas.DataFrame(
                 dict(x=(xg + dx).flatten(), y=yg.flatten(), frame=t))
+
         inity = np.arange(4)
-        initprof = np.vstack((inity, inity*0.45)).T
+        initprof = np.vstack((inity, inity*0.0045)).T
         # We need a weird bin size (1.1) to avoid bin boundaries coinciding
         # with particle positions.
         pred = predict.ChannelPredict(1.1, minsamples=3,
                                       initial_profile_guess=initprof)
         # print(_shear_frame(1.))
-        ll = get_linked_lengths((_shear_frame(0), _shear_frame(1.),
-                                 _shear_frame(2), _shear_frame(3)),
+        ll = get_linked_lengths((_shear_frame(0), _shear_frame(100),
+                                 _shear_frame(200), _shear_frame(300)),
                         self.get_linker_iter(pred), 0.45)
         assert all(ll.values == 4)
+
 
 class ChannelPredictYTests(VelocityPredictTests, StrictTestCase):
     def setUp(self):
@@ -233,10 +239,11 @@ class ChannelPredictYTests(VelocityPredictTests, StrictTestCase):
             predict.instrumented()(predict.ChannelPredict),
             3, 'y', minsamples=3)
         self.mkframe = self._channel_frame
+
     def _channel_frame(self, n=1, Nside=3):
         xg, yg = np.mgrid[:Nside,:Nside]
         dx = 0.
-        dy = (n - 1) * np.sqrt(2)
+        dy = (n / 100 - 1) * np.sqrt(2)
         return pandas.DataFrame(
             dict(x=xg.flatten() + dx, y=yg.flatten() + dy, frame=n))
 
