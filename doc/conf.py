9408b3215e8ce3c809d487956ba37d9a89f8d540
Casper van der Wel <caspervdw@gmail.com>
2017-04-22 18:07:25 +0200

    Update copyright year in docs

--- doc/conf.py
+++ doc/conf.py
@@ -61,9 +61,11 @@ source_suffix = '.rst'
 # The master toctree document.
 master_doc = 'index'
 
+from datetime import date
+
 # General information about the project.
 project = u'trackpy'
-copyright = u'2010-2014, Trackpy Contributors'
+copyright = u'2010-{}, Trackpy Contributors'.format(date.today().year)
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
