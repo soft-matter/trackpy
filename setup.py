511b1326d53a9883bcb17ed36d5e03531b4d85e2
Nathan Keim <nkeim@calpoly.edu>
2019-09-24 12:00:21 -0700

    BLD: Add locate_functions to setup.py

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup_parameters = dict(
     url = "https://github.com/soft-matter/trackpy",
     install_requires = ['numpy>=1.8', 'scipy>=0.12', 'six>=1.8',
 	                'pandas>=0.13', 'pyyaml', 'matplotlib'],
-    packages = ['trackpy', 'trackpy.refine', 'trackpy.linking'],
+    packages = ['trackpy', 'trackpy.refine', 'trackpy.linking', 'trackpy.locate_functions'],
     long_description = descr,
 )
 
